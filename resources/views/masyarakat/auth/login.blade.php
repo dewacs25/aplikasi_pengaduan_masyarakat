09ceb9aac8a64eda592d71f44adac6b424e25780
csdewa25 <csdewa25@gmail.com>
2023-04-08 12:13:02 +0700

    update style

--- resources/views/masyarakat/auth/login.blade.php
+++ resources/views/masyarakat/auth/login.blade.php
@@ -13,21 +13,23 @@
     <div class="container-login">
         <form class="login-form" action="{{ route('login') }}" method="POST">
             @csrf
-          <h1>Login</h1>
-        @if ($errors->any())
-            @foreach ($errors->all() as $er)
-                <p style="font-size: 12px; color: red">{{ $er }}</p>
-            @endforeach
-        @endif
-          <label for="username">Username:</label>
-          <input type="text" name="username" placeholder="Username" >
+            <h1>Login</h1>
+            @if ($errors->any())
+                @foreach ($errors->all() as $er)
+                    <p style="font-size: 12px; color: red">{{ $er }}</p>
+                @endforeach
+            @endif
+            <label for="username">Username:</label>
+            <input type="text" name="username" placeholder="Username">
 
-          <label for="password">Password:</label>
-          <input type="text" name="password" placeholder="Password" >
+            <label for="password">Password:</label>
+            <input type="text" name="password" placeholder="Password">
 
-          <button type="submit">Submit</button>
+            <button type="submit">Login</button>
+            <p style="font-size: 12px">Belum Punya Akun ? <a href="/register">Daftar Sekarang</a></p>
         </form>
-      </div>
+
+    </div>
 
 </body>
 
--- resources/views/masyarakat/auth/register.blade.php
+++ resources/views/masyarakat/auth/register.blade.php
@@ -5,28 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/stylefront.css') }}">
 </head>
 
-<body class="bg-admin">
+<body>
 
-    <div class="card-login">
-        <h3 class="text-center text-primary">Login</h3>
-        @if ($errors->any())
+    <div class="container-login">
+        <form action="{{ route('register.masyarakat') }}" class="login-form" method="POST">
+            <h1>Register</h1>
+            @if ($errors->any())
             @foreach ($errors->all() as $er)
-                <p style="font-size: 12px; color: red">{{ $er }}</p>
+            <p style="font-size: 12px; color: red">{{ $er }}</p>
             @endforeach
-        @endif
-        <form action="{{ route('register.masyarakat') }}" method="POST">
+            @endif
             @csrf
-            <input type="text" name="nik" placeholder="Nik" class="form-control mb-3">
-            <input type="text" name="nama" placeholder="Nama" class="form-control mb-3">
-            <input type="text" name="username" placeholder="Username" class="form-control mb-3">
+            <input type="text" name="nik" placeholder="Nik">
+            <input type="text" name="nama" placeholder="Nama">
+            <input type="text" name="username" placeholder="Username">
             
-            <input type="password" name="password" placeholder="Password" class="form-control mb-3">
-            <input type="number" name="telp" placeholder="No Telepon" class="form-control mb-3">
+            <input type="password" name="password" placeholder="Password">
+            <input type="number" name="telp" placeholder="No Telepon">
+            <button type="submit">Register</button>
             <center>
-                <button class="btn btn-primary mb-3">Register</button>
                 <br>
                 <span class="text-danger" style="font-size: 12px; color:rgb(88, 88, 88);">
                     Sudah Punya Akun Silakan <a href="/login" style="color: red">Login Disini</a>
