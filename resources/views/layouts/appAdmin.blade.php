de228bce2b5d90455e2a13b3d8449588a9f7ee2a
csdewa25 <csdewa25@gmail.com>
2023-04-09 19:29:44 +0700

    update

--- resources/views/layouts/appAdmin.blade.php
+++ resources/views/layouts/appAdmin.blade.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <title>Admin</title>
     </head>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
